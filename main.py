14559c243631ac583090d15b0efe8be528be5c81
jaegray <jae_gray@yahoo.com>
2022-01-03 10:48:26 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Safari  # installing browser drive
+
+driver = Safari()  # creating an instance of the browser
+driver.get("https://www.ziprecruiter.com/Salaries/Cloud-Engineer-Salary")
+assert "Salary" in driver.title
+
