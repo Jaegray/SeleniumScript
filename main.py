07020a7841830eb3a2480ca0bb731061b2ece265
Jae <82623058+Jaegray@users.noreply.github.com>
2022-01-07 01:56:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from selenium.webdriver import Safari  # installing browser drive
 
 driver = Safari()  # creating an instance of the browser
 driver.get("https://www.ziprecruiter.com/Salaries/Cloud-Engineer-Salary")
-assert "Salary" in driver.title
+assert "Salary" in driver.title  # command
 
